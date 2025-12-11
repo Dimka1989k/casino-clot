725e29702d4614f9833cc0e269c55913a7f752ba
Dmytro Kyselov <dima1989k@ukr.net>
2025-12-11 14:47:59 +0200

    change ui

--- app/components/SlotMachine.tsx
+++ app/components/SlotMachine.tsx
@@ -3,9 +3,6 @@
 import { motion } from "framer-motion";
 import { useSlotLogic } from "@/app/hooks/useSlotLogic";
 import { Reel } from "@/app/components/Reel";
-import { BetControls } from "@/app/components/BetControls";
-import { SpinButton } from "@/app/components/SpinButton";
-import { Balance } from "@/app/components/Balance";
 import { WinModal } from "@/app/components/WinModal";
 import { LoseModal } from "@/app/components/LoseModal";
 
@@ -43,10 +40,7 @@ export function SlotMachine() {
                 />
               ))}
             </div>
-          </div>
-          <BetControls />
-          <SpinButton />
-          <Balance />
+          </div>       
         </div>
         <WinModal open={modalVisible && gameResult === "win"} />
         <LoseModal open={modalVisible && gameResult === "lose"} />
